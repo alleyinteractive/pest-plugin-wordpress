a080422c12b15910e02d8cbed76eaeeaa6229883
Renato Alves <19148962+renatonascalves@users.noreply.github.com>
2022-08-16 14:03:00 -0300

    CR suggestion

--- src/Autoload.php
+++ src/Autoload.php
@@ -2,10 +2,4 @@
 
 declare(strict_types=1);
 
-if (file_exists(dirname(__DIR__, 3) . '/wordpress-autoload.php')) {
-    require_once dirname(__DIR__, 3) . '/wordpress-autoload.php';
-} else {
-    require_once __DIR__ . '/../vendor/wordpress-autoload.php';
-}
-
 require_once __DIR__ . '/Http.php';
